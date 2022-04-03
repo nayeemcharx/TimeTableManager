bee904fa7791682e605a64ae205d010b68bdba69
nayeemcharx <n.nayeem.rm@gmail.com>
2022-04-03 16:01:03 +0600

    adding necessary activities

--- app/src/main/java/com/example/timetablemanager/MainActivity.kt
+++ app/src/main/java/com/example/timetablemanager/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.timetablemanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dayViewText: TextView
+    private lateinit var weekViewText: TextView
+    private lateinit var AddTaskButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //starting
+        dayViewText=findViewById(R.id.dayview)
+        weekViewText=findViewById(R.id.weekview)
+        AddTaskButton=findViewById(R.id.add_task_button)
+        dayViewText.setOnClickListener{
+
+            val intent = Intent(this,DayViewActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        weekViewText.setOnClickListener{
+
+            val intent = Intent(this,WeekViewActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        AddTaskButton.setOnClickListener{
+            val intent = Intent(this,TaskEditorActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/timetablemanager/WeekViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.timetablemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeekViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_week_view)
+    }
+}
